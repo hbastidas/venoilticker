0b0d6f0a377e3203460f3fa195dcead6baf65291
Heber Bastidas <heberbastidas@gmail.com>
2018-08-04 17:08:08 -0400

    create scraping general lib

--- driver/mpetromin.js
+++ driver/mpetromin.js
@@ -1,16 +1,16 @@
-const cheerio = require('cheerio');
-const cheerioTableparser = require('cheerio-tableparser');
-const axios = require('axios');
+const Scraping = require('../lib/scraping');
 
 // Define Class
 function Mpetromin() {
   this.dates = undefined;
   this.db = [];
-
-  this.run();
+  //get petrocoin price for oil price
+  this.scraping = new Scraping('http://www.minpet.gob.ve:81/visew/petro.php', '#fuente table');
 }
 
 Mpetromin.prototype.extractor = function(data) {
+
+  console.log(data);
   var tempindex = "";
 
   for (var dt in data) {
@@ -48,23 +48,8 @@ Mpetromin.prototype.extractor = function(data) {
   this.db = tempdb;
 }
 
-Mpetromin.prototype.run = function() {
-    this.scraping();
-};
-
-Mpetromin.prototype.deamon = function() {
-  self = this;
-  setInterval(function() {
-    self.run();
-  }, 10000)
-};
-
-Mpetromin.prototype.scraping = async function(callback) {
-  const mpetrominres = axios('http://www.mpetromin.gob.ve/portalmenpet/secciones.php?option=view&idS=45');
-  const [response] = await Promise.all([mpetrominres]);
-  let $ = cheerio.load(response.data);
-  cheerioTableparser($);
-  this.extractor($('#contenido table').parsetable(true, true, true));
+Mpetromin.prototype.run = async function() {
+    this.extractor(await this.scraping.get().catch(error =>{ throw error;}));
 };
 
 Mpetromin.prototype.getdata = function() {
--- test.js
+++ test.js
@@ -3,11 +3,19 @@ let Venoilticker = require('./index');
 // Start Libribbit
 let venoilticker = new Venoilticker();
 
-setInterval(function(){
+//running webscraping
+let running=setInterval(function(){
+  venoilticker.source['mpetromin'].run().catch(error=>{
+    clearInterval(running);
+    console.log(error)
+  })
+
   let data = venoilticker.source['mpetromin'].getdata();
+
   if (Object.keys(data).length!=0) {
     console.log(data);
   }else {
     console.log("recibiendo datos....");
   }
+
 }, 15000)
