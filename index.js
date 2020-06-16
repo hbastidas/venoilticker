68b06aa1a7742b21821aadfb5d64361c0a443e53
Heber Bastidas <heberbastidas@gmail.com>
2020-06-16 17:44:45 +0200

    remove minpet Venezuelan oil price

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var Mpetromin = require('./driver/mpetromin');
 var Inech = require('./driver/inecn');
 
 function Venoilticker(){}
 
 Venoilticker.prototype.source = {
-	mpetromin: new Mpetromin(),
 	inecn: new Inech()
 };
 
